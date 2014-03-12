7e9d050b69efc04b7ccf300b2de52a2891984b50
BlackNoxis <steven.darklight@gmail.com>
2014-03-12 05:41:56 +0200

    oops, error

--- servers.sh
+++ servers.sh
@@ -2,4 +2,4 @@
 
 /bin/echo "rogentosuser:G3ntoo123" | chpasswd
 /bin/ifconfig eth0 192.168.2.10 netmask 255.255.255.0 up
-/binroute add default gw 192.168.2.1
+/bin/route add default gw 192.168.2.1
